9f79a17d9ad9389b07a5a7ee8d99d19c991ddda3
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-10-19 09:26:00 +0300

    Add not found handling page

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,10 @@
+const NewsNotFoundPage = () => {
+    return (
+        <div id="error">
+            <h1>Not Found!</h1>
+            <p>Unfortunately, We could not find the requested artical</p>
+        </div>
+    )
+}
+
+export default NewsNotFoundPage
